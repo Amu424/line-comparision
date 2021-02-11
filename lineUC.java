2b91553d0eae7e24a36f3e6944ebcdaa2f4b0703
softtech <amols424@gmail.com>
2021-02-11 11:14:25 +0530

    Added UC1

--- lineUC.java
+++ lineUC.java
@@ -1,6 +1,15 @@
+import java.lang.Math.*;
 class lineUC{
 public static void main(String[] args){
-System.out.println("Welcome to line comparision");
+int x1=5;
+int y1=10;
+int x2=15;
+int y2=20;
+double l=((x2-x1)^2+(y2-y1)^2);
+int len=(int)l;
+int length=(int)(Math.sqrt(len));
+
+System.out.println("length of line is"+ length);
 
 
 }
