e08cab29d8da269dc77f822d0277a8a5bf79a81b
softtech <amols424@gmail.com>
2021-02-11 19:46:25 +0530

    Added line are equal or not

--- lineUC.java
+++ lineUC.java
@@ -5,12 +5,25 @@ int x1=5;
 int y1=10;
 int x2=15;
 int y2=20;
-double l=((x2-x1)^2+(y2-y1)^2);
-int len=(int)l;
-int length=(int)(Math.sqrt(len));
 
-System.out.println("length of line is"+ length);
+int x3=10;
+int y3=20;
+int x4=15;
+int y4=30;
+double l1=((x2-x1)^2+(y2-y1)^2);
+double l2=((x4-x3)^2+(y4-y3)^2);
+int len2=(int)l2;
+int len1=(int)l1;
+int length1=(int)(Math.sqrt(len1));
+int length2=(int)(Math.sqrt(len2));
 
+System.out.println("length of line 1 is"+ length1+"and 2nd line length is"+length2);
+if(length1==length2){
+System.out.println("lines are equal");
+}
+else{
+System.out.println("lines are not equal");
+}
 
 }
 }
